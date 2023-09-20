4f7d14b6654ad246e91e8deac0f3df50d4382ecd
bachhhhe160470 <bachhhhe160470@fpt.edu.vn>
2023-09-20 23:23:42 +0700

    resolve cofiguration

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  //...
+  resolve: {
+    alias: {
+      Views: path.resolve(__dirname, "src/views/"),
+      Assets: path.resolve(__dirname, "src/assets/"),
+      Services: path.resolve(__dirname, "src/services/"),
+      Components: path.resolve(__dirname, "src/components/"),
+      Utils: path.resolve(__dirname, "src/utils/"),
+
+    },
+  },
+};
